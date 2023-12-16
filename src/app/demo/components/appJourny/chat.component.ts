4afb4fa750157ced0a6853ac1b7c50f056d7e47b
amroNahas <amrelnahas4@gmail.com>
2023-12-16 17:22:33 +0300

    with audio

--- src/app/demo/components/appJourny/chat.component.ts
+++ src/app/demo/components/appJourny/chat.component.ts
@@ -40,8 +40,9 @@ export class ChatComponent {
         this.messgesList = []
         if (this.userInfo)
             this.allowedToSend = true;
-         this.srcInit ="https://d-id-talks-prod.s3.us-west-2.amazonaws.com/auth0%7C657d5fd688c5a1a2857188bf/tlk_6W4IRfXyWeNo5LykgfcW8/1702727250134.mp4?AWSAccessKeyId=AKIA5CUMPJBIK65W6FGA&Expires=1702813761&Signature=2UupWqFBstn8Exbd%2BJMVSmfsAAA%3D&X-Amzn-Trace-Id=Root%3D1-657d8ec1-46e2dc056a9a95cf7ec8a989%3BParent%3D6163dae35dbc3d8b%3BSampled%3D0%3BLineage%3D6b931dd4%3A0"
-       // this.getVideoInit  ("Welcome "+this.userInfo.fullName+" To Nusuk Ai Advisor , How Can  I  Help You  For Your Nusuk Journy ")
+        this.srcInit = "https://d-id-talks-prod.s3.us-west-2.amazonaws.com/auth0%7C657d5fd688c5a1a2857188bf/tlk_6W4IRfXyWeNo5LykgfcW8/1702727250134.mp4?AWSAccessKeyId=AKIA5CUMPJBIK65W6FGA&Expires=1702813761&Signature=2UupWqFBstn8Exbd%2BJMVSmfsAAA%3D&X-Amzn-Trace-Id=Root%3D1-657d8ec1-46e2dc056a9a95cf7ec8a989%3BParent%3D6163dae35dbc3d8b%3BSampled%3D0%3BLineage%3D6b931dd4%3A0"
+        this.videoShow = true;
+        // this.getVideoInit  ("Welcome "+this.userInfo.fullName+" To Nusuk Ai Advisor , How Can  I  Help You  For Your Nusuk Journy ")
 
     }
 
@@ -72,39 +73,45 @@ export class ChatComponent {
     audio: string;
     msgDto: MessagesDto = new MessagesDto();
     srcInit: string;
-
+    videoShow: boolean;
 
     send() {
-        this.allowedToSend = false;
-        this.scrollToBottom()
-        this.msgDto = new MessagesDto();
-        this.msgDto.msgFrom = this.messageFrom;
-        this.msgDto.appAdvisorName = "Nusuk Advisor AI"
-        this.msgDto.userName = this.userInfo.fullName
-        this.messgesList.push(this.msgDto);
-        this.messageFrom = "";
-        this.gbtService.sendMsg(this.msgDto.msgFrom, this.threadId).subscribe({
-            next: (res: GbtMsgResp) => {
-                if (res && res.chatResponse) {
-                    this.threadId = res.threadId;
-                    this.msgDto.msgTo = res.chatResponse;
+        if (!this.messageFrom || !this.allowedToSend) {
+
+            return
+        } else {
+            this.allowedToSend = false;
+            this.videoShow = false;
+            this.scrollToBottom()
+            this.msgDto = new MessagesDto();
+            this.msgDto.msgFrom = this.messageFrom;
+            this.msgDto.appAdvisorName = "Nusuk Advisor AI"
+            this.msgDto.userName = this.userInfo.fullName
+            this.messgesList.push(this.msgDto);
+            this.messageFrom = "";
+            this.gbtService.sendMsg(this.msgDto.msgFrom, this.threadId).subscribe({
+                next: (res: GbtMsgResp) => {
+                    if (res && res.chatResponse) {
+                        this.threadId = res.threadId;
+                        this.msgDto.msgTo = res.chatResponse;
+                        this.allowedToSend = true;
+                        this.textToAudio(this.msgDto.msgTo)
+                        this.getVideo(this.msgDto.msgTo);
+
+                    }
+                },
+                error: (err) => {
                     this.allowedToSend = true;
-                    this.textToAudio(this.msgDto.msgTo)
-                    this.getVideo(this.msgDto.msgTo);
-
-                }
-            },
-            error: (err) => {
-                this.allowedToSend = true;
-                console.log(err);
-            },
-        });
+                    console.log(err);
+                },
+            });
 
 
-        localStorage.setItem("my chat", JSON.stringify(this.messgesList));
-        /*        setTimeout(() => {
-                    msgDto.msgTo="fghjkjhgfghjk sfsssf  sfsfsfsfsfsfsf sfsdfsfsff"
-                }, 1000);*/
+            localStorage.setItem("my chat", JSON.stringify(this.messgesList));
+            /*        setTimeout(() => {
+                        msgDto.msgTo="fghjkjhgfghjk sfsssf  sfsfsfsfsfsfsf sfsdfsfsff"
+                    }, 1000);*/
+        }
     }
 
     textToAudio(msg) {
@@ -127,7 +134,9 @@ export class ChatComponent {
         this.gbtService.textToVideo(msg, null).subscribe({
             next: (res: any) => {
                 if (res) {
+                    this.srcInit = res.text;
                     this.msgDto.videoSrc = res.text;
+                    this.videoShow = true;
                 }
             },
             error: (err) => {
